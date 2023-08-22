e455ee03513e7f69af668d727cb2a55b1e0e407c
Alfajri <alfajri607@gmail.com>
2023-08-23 03:03:37 +0700

    update

--- spring-cloud-mapping/src/main/resources/seeder/seeder.sql
+++ spring-cloud-mapping/src/main/resources/seeder/seeder.sql
@@ -1,4 +1,4 @@
 -- insert the administrator
-INSERT IGNORE INTO zupay.users(id,email,created_at,updated_at,username,password) VALUES ('dshjdsdkfds','admin@admin.com','2022-01-01','2022-01-01','admin@admin.com','$2a$10$Ie43.ByI9FdQmiwxmgbBWOlKnbMgtoyimHvNawPTr8BqXWqvhvE72');
-INSERT IGNORE INTO zupay.source_api(id,name,created_at,updated_at) VALUES (1,'reloadly','2022-01-01','2022-01-01');
+INSERT INTO cantikapp.users(id,email,created_at,updated_at,username,password) VALUES ('dshjdsdkfds','admin@admin.com','2022-01-01','2022-01-01','admin@admin.com','$2a$10$Ie43.ByI9FdQmiwxmgbBWOlKnbMgtoyimHvNawPTr8BqXWqvhvE72');
+INSERT INTO cantikapp.source_api(id,name,created_at,updated_at) VALUES (1,'reloadly','2022-01-01','2022-01-01');
 
